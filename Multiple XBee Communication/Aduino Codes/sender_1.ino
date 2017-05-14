ca3e4e59337a2b1dc9b36ad8571e3f6d64a28acc
baruaranojoy <baruaranojoy1@gmail.com>
2017-05-14 15:57:43 +0530

    Add files via upload

---
+++ Multiple XBee Communication/Aduino Codes/sender_1.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String ch = "Hello World" ;
+  Serial.println(ch);
+  delay(1000);
+}
